f23fb95241956a7ca4e2da3b906e788dc53848ca
sakibnjr <110890368+sakibnjr@users.noreply.github.com>
2022-08-09 16:24:49 +0600

    Add files via upload

---
+++ problem3.java
@@ -0,0 +1,10 @@
+public class problem3 {
+    public static void main(String[] args) {
+        float a = 3.5F;
+        float b = 7.2F;
+
+        float c = a * b;
+        System.out.println("Product = " + c);
+
+    }
+}
