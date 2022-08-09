a1382c74f6aa70594b232b74220e682fe52fc56a
sakibnjr <110890368+sakibnjr@users.noreply.github.com>
2022-08-09 16:23:50 +0600

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        int num;
+        System.out.println("Enter any number :");
+        num = src.nextInt();
+        System.out.println("You have entered = " + num);
+    }
+}
